089f9fe26eee1482adaa6e960c4ff6a8336085dd
MoonshineSG <ovidiu@binary.local>
2016-01-04 14:13:23 +0800

    added update hook

--- octoprint_active_filters/__init__.py
+++ octoprint_active_filters/__init__.py
@@ -7,6 +7,34 @@ class ActiveFiltersPlugin(octoprint.plugin.AssetPlugin):
 			js=["js/knockout.persist.js", "js/active_filters.js"]
 		)
 
+	def get_version(self):
+		return self._plugin_version
+
+	def get_update_information(self):
+		return dict(
+			touchui=dict(
+				displayName="Active Filters",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="MoonshineSG",
+				repo="OctoPrint-ActiveFilters",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/MoonshineSG/OctoPrint-ActiveFilters/archive/{target_version}.zip"
+			)
+		)
+
 __plugin_name__ = "Active Filters"
-__plugin_implementation__ = ActiveFiltersPlugin()
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = ActiveFiltersPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
 
