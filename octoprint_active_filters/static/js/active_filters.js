0ab8eb7c9d6b3b132140412cc57fb9f8d645d0fc
ǝuıɥsuooɯ <moonshinesg@me.com>
2017-05-12 10:51:56 +0800

    cleanup potential manually removed filters

--- octoprint_active_filters/static/js/active_filters.js
+++ octoprint_active_filters/static/js/active_filters.js
@@ -3,7 +3,15 @@ $(function() {
 		var self = this;
 		
 		self.onAfterBinding = function () {
-			terminal = viewModels[0];			
+			terminal = viewModels[0];
+			//cleanup potential manually removed filters
+			currentFilters = terminal.filters();
+			savedValues = JSON.parse(localStorage.getItem('terminal.activeFilters'));
+			cleanValues = _.filter(savedValues, function(value) {
+					return currentFilters.some(function(e) { return e.regex == value; } );
+			});
+			localStorage.setItem('terminal.activeFilters', ko.toJSON(cleanValues));
+			
 			terminal.activeFilters = terminal.activeFilters.extend({ persist: 'terminal.activeFilters' });
 		}
 	}
